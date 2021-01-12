cbee5e7525cdb2d751cd9aa92aaa9035ce69db1d
amoonlessnight <shannyn.boyd@gmail.com>
2021-01-12 21:34:57 +1300

    Added additional lines about favourite day of week

--- PartD/src/main.java
+++ PartD/src/main.java
@@ -5,6 +5,7 @@ public class main {
 		System.out.println("Hi, my name is Shannyn.");
 		System.out.println("My favourite colour is black.");
 		System.out.println("My favourite mode of transportation is walking and biking.");
+		System.out.println("The best day of the week is Saturday.");
 	}
 
 }
