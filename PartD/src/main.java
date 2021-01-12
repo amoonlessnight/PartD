b9ab4388085fcee61cdf194462a539b9af092ad3
amoonlessnight <shannyn.boyd@gmail.com>
2021-01-12 21:33:48 +1300

    Added additional lines with favourite transport mode

--- PartD/src/main.java
+++ PartD/src/main.java
@@ -4,6 +4,7 @@ public class main {
 	public static void main(String[] args) {
 		System.out.println("Hi, my name is Shannyn.");
 		System.out.println("My favourite colour is black.");
+		System.out.println("My favourite mode of transportation is walking and biking.");
 	}
 
 }
